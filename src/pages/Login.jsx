e9cb83eb22fdc374c62277bc61416d3352a6b5b4
MD-Aftab-Hossain <md.aftab.hn@gmail.com>
2025-11-13 21:16:10 +0600

    last of this comment

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -22,7 +22,6 @@ const Login = () => {
       .catch(err => {
         console.log(err)
         toast.error(`${err}`)
-        // setmsg('Invalid Email or Password')
       })
   }
   const handlepopup = () => {
